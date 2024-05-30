0d8f7b232b779b3603684d3482c1001d2c279be6
Saajvatz <ajvatzalejandro@gmail.com>
2024-05-30 12:03:33 -0600

    header y footer

--- footer/footer.js
+++ footer/footer.js
@@ -1,3 +1,13 @@
 let footer_DOM = document.querySelector(".footer");
 
-footer_DOM.innerHTML = `hola soy footer`;
+footer_DOM.innerHTML = `
+<div class="footer">
+
+<h1> saajvatz@scl.edu.gt</h1>
+<h1> davillaroto@csl.edu.gt</h1>
+<h1> pagiron@scl.edu.gt</h1>
+<h1> macalderas@scl.edu.gt</h1>
+
+</div>
+<img src="https://scontent.fgua4-1.fna.fbcdn.net/v/t39.30808-6/238026428_4490684491017777_8763802531022229176_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=5f2048&_nc_ohc=H1gYG4H_wR0Q7kNvgHeN4NT&_nc_ht=scontent.fgua4-1.fna&oh=00_AYDxCBVc2LERD9W8ALYYtSeWeCldavlw3zALOaDsi3pvLw&oe=665E9449" alt="">
+`;
--- header/header.js
+++ header/header.js
@@ -1,4 +1,6 @@
 let header_DOM = document.querySelector(".header");
 
-header_DOM.innerHTML = `hola soy header;
-`
+header_DOM.innerHTML = `
+<h1 class="titulo" >Gramatica HTML </h1>
+<a href="https://github.com/Saajvatz/etiquetas_html" class="btn-github">GRID</a>
+`;
